e022ff2c08db814d899107ce9fbdc63faee0e30c
Lonka-Pardhu <lonkapardhu99@gmail.com>
2023-12-11 16:53:11 +0100

    js problems

--- String-problems/count-vowel-consonants/count-v-c.js
+++ String-problems/count-vowel-consonants/count-v-c.js
@@ -1,8 +1,44 @@
+//* Solution-1
 function countVowelsAndConsonants(string) {
-    const vowelsAndConsonants = {};
 
+    const filteredStr = string.toLowerCase();
+
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+    var vowelsAndConsonants = { vowels: 0, consonants: 0 }
+
+    for (let char of filteredStr) {
+        if (vowels.includes(char)) {
+            vowelsAndConsonants['vowels']++;
+        }
+        else if (char !== " ") {
+            vowelsAndConsonants['consonants']++;
+        }
+    }
+    return vowelsAndConsonants;
 }
 
-const inputString = 'Hello world';
+const inputString = 'Pardhu';
+console.log(countVowelsAndConsonants(inputString));
 
-console.log(countVowelsAndConsonants(inputString))
+
+//* Solution-2
+
+// function countVowelsAndConsonants(string) {
+
+//     const filteredStr = string.toLowerCase();
+
+//     const vowelsRegex = /[aeiou]/
+
+//     var vowelsAndConsonants = { vowels: 0, consonants: 0 }
+
+//     for (let char of filteredStr) {
+//         if (vowelsRegex.test(char)) {
+//             vowelsAndConsonants['vowels']++;
+//         }
+//         else if (char !== " ") {
+//             vowelsAndConsonants['consonants']++;
+//         }
+//     }
+//     return vowelsAndConsonants;
+// }
